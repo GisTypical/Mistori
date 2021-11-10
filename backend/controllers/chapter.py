1bbb68810b17aca5b49152b102e9cf9d56021b29
Gerardo <gerardopapaleco@gmail.com>
2021-11-10 03:42:58 -0400

    pages upload & get pages

--- backend/controllers/chapter.py
+++ backend/controllers/chapter.py
@@ -3,14 +3,38 @@ from flask import Blueprint, request
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 
+from models.chapter import Chapter
+from cloudinary import uploader
+from cloudinary import api
+
 chapter_bp = Blueprint('chapter_bp', __name__)
+
 
 @chapter_bp.route('/chapter', methods=['POST'])
 @jwt_required()
 def create_chapter():
-    print(request.form['chapterName'])
+    data = request.form
 
-    for uploaded_img in request.files.getlist('pagesImgs'):
-        print(uploaded_img.filename)
-        uploaded_img.save(uploaded_img.filename)
-    return {}, 200
+    if not data:
+        return {}, 400
+
+    for index, page in enumerate(request.files.getlist('pages'), start=1):
+        print(index, page.filename)
+        uploader.upload_image(
+            page, folder=f'mistori/{data["mangaId"]}/{data["title"]}', public_id=index)
+
+    chapter = Chapter(
+        title=data['title'], date=data['date'], manga_id=data['mangaId'], pages=f'mistori/{data["mangaId"]}')
+
+    db.session.add(chapter)
+    db.session.commit()
+
+    return {'message': 'Chapter created'}, 201
+
+
+@chapter_bp.route('/chapter/<string:pic_id>', methods=['GET'])
+@jwt_required()
+def chapter_pages(pic_id):
+    chapter = Chapter.query.filter_by(id=pic_id).first()
+    pages = api.resources(type="upload", prefix=chapter.pages)
+    return pages, 200
