fbc13484297e9fcc760ccf9de7ddc5ec8b183af2
Gerardo <gerardopapaleco@gmail.com>
2021-11-09 22:35:45 -0400

    comment parent column

--- backend/models/comment.py
+++ backend/models/comment.py
@@ -8,7 +8,8 @@ class Comment(db.Model):
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     date = db.Column(db.DateTime, default=datetime.now, nullable=False)
-    parent = db.relationship('Comment', backref='comment', remote_side=[id])
     text = db.Column(db.Text, nullable=False)
+    parent_id = db.Column(UUID(as_uuid=True), db.ForeignKey('comment.id'))
     username = db.Column(db.String, db.ForeignKey('user_account.username', ondelete='cascade'), nullable=False)
     chapter_id = db.Column(UUID(as_uuid=True), db.ForeignKey('chapter.id', ondelete='cascade'), nullable=False)
+    parent = db.relationship('Comment', backref='comment', remote_side=[id])
