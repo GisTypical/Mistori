eb14b55d2441ed48f9192f65c7c618be9d80659d
Gerardo <gerardopapaleco@gmail.com>
2021-11-09 20:17:07 -0400

    change from String to Text type

--- backend/models/comment.py
+++ backend/models/comment.py
@@ -9,6 +9,6 @@ class Comment(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     date = db.Column(db.DateTime, default=datetime.now, nullable=False)
     parent = db.relationship('Comment', backref='comment', remote_side=[id])
-    text = db.Column(db.String, nullable=False)
+    text = db.Column(db.Text, nullable=False)
     username = db.Column(db.String, db.ForeignKey('user_account.username', ondelete='cascade'), nullable=False)
     chapter_id = db.Column(UUID(as_uuid=True), db.ForeignKey('chapter.id', ondelete='cascade'), nullable=False)
