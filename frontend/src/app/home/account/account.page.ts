53f0b9211db00330c6099165a6d2193b164d045c
Nathalie <nathalievzm@hotmail.com>
2021-11-10 23:43:12 -0400

    get mangas done

--- frontend/src/app/home/account/account.page.ts
+++ frontend/src/app/home/account/account.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { MangaService } from '../../services/manga.service'
+import { Manga } from  '../../shared/Manga'
 
 @Component({
   selector: 'app-account',
@@ -9,14 +11,15 @@ import { AuthService } from 'src/app/services/auth.service';
 export class AccountPage implements OnInit {
   isLogged: string;
   isLoading: boolean;
+  mangas: Manga[] = []
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private mangaService: MangaService) {
     this.authService.isLogged.subscribe((s) => (this.isLogged = s));
   }
 
   ngOnInit() {}
 
   ionViewDidEnter() {
-    console.log('Hi')
+    this.mangaService.getUploadedMangas().subscribe((mangas) => this.mangas = mangas.mangas)
   }
 }
