2dc79d5cdd95ef2fccbcf9a054f5d6219b59d8bd
Gerardo <gerardopapaleco@gmail.com>
2021-11-11 15:19:38 -0400

    last little changes in ux

--- frontend/src/app/pages/login/login.page.ts
+++ frontend/src/app/pages/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import { finalize } from 'rxjs/operators';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../shared/User';
 
@@ -9,16 +11,44 @@ import { User } from '../../shared/User';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+  isError = false;
+  private loading: HTMLIonLoadingElement;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private loadingController: LoadingController
+  ) {}
 
   ngOnInit() {}
 
   loginUser(user: User) {
-    this.authService.userLogin(user).subscribe((data) => {
-      localStorage.setItem('accessToken', data.accessToken);
-      localStorage.setItem('refreshToken', data.refreshToken);
-      this.authService.setLogged('logged');
-      this.router.navigate(['/home']);
+    this.isError = false;
+    this.presentLoading();
+    this.authService
+      .userLogin(user)
+      .pipe(
+        finalize(() => {
+          this.loading.dismiss();
+        })
+      )
+      .subscribe(
+        (data) => {
+          localStorage.setItem('accessToken', data.accessToken);
+          localStorage.setItem('refreshToken', data.refreshToken);
+          this.authService.setLogged('logged');
+          this.router.navigate(['/home']);
+        },
+        () => {
+          this.isError = true;
+        }
+      );
+  }
+
+  async presentLoading() {
+    this.loading = await this.loadingController.create({
+      message: 'Please wait...',
     });
+    return this.loading.present();
   }
 }
