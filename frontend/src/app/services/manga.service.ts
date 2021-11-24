f7668714505ad83dcb7a6e41b8769f233f40cef0
Gerardo <gerardopapaleco@gmail.com>
2021-11-24 00:05:55 -0400

    update user/mangas on create manga

--- frontend/src/app/services/manga.service.ts
+++ frontend/src/app/services/manga.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment } from '../../environments/environment';
 import { Manga, MangasObject } from '../shared/Manga';
@@ -11,16 +11,23 @@ import { Message } from '../shared/Message';
 })
 export class MangaService {
   currentMangaID: Observable<string>;
+  getMangaEvent: Observable<unknown>;
 
   private mangaID = new BehaviorSubject('');
+  private addMangaEvent = new Subject();
   private apiURL = environment.apiUrl;
 
   constructor(private http: HttpClient) {
     this.currentMangaID = this.mangaID.asObservable();
+    this.getMangaEvent = this.addMangaEvent.asObservable();
   }
 
   setManga(mangaID: string) {
     this.mangaID.next(mangaID);
+  }
+
+  sendMangaAddEvent() {
+    this.addMangaEvent.next();
   }
 
   submitManga(formData: FormData): Observable<Message> {
