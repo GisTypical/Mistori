ccd396d983a2282d0d0cd30f96729c519f14d056
Gerardo <gerardopapaleco@gmail.com>
2021-11-24 00:03:13 -0400

    parseDate util

---
+++ frontend/src/app/utils/parseDate.ts
@@ -0,0 +1,10 @@
+import { Manga } from '../shared/Manga';
+
+export const parseDate = (mangas: Manga[]) => {
+  for (const manga of mangas) {
+    const mangaYear = new Date(manga.date).getFullYear();
+    const mangaMonth = new Date(manga.date).getMonth();
+    const mangaDay = new Date(manga.date).getDate();
+    manga.date = `${mangaMonth}/${mangaDay}/${mangaYear}`;
+  }
+};
